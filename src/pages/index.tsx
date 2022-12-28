e35196369f0c9c9d3ca871a420b888852e2adddb
akramk295 <75031721+Shoaib2472@users.noreply.github.com>
2022-12-28 21:12:50 +0530

    added custom css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,6 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
     </>
   );
 };
